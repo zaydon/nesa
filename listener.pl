d0ddf7979f9504280df5ef566278f654fd5d4203
Tobias Leich <email@froggs.de>
2013-12-10 11:33:21 +0100

    added simple listener

---
+++ listener.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use feature qw(say);
+use IO::Socket::INET;
+
+my $localport = 7030;
+my $server    = IO::Socket::INET->new(
+    LocalPort => $localport,
+    Type      => SOCK_STREAM,
+    Reuse     => 1,
+    Listen    => 10 )
+    or die "Unable to spawn server on port $localport: $!\n";
+
+while ($client = $server->accept()) {
+    # $client is the new connection
+    say my $answer = <$client> =~ s/\R\z//r;
+}
+
+close($server);
